7c0841f5ed18655ea96f8a064ae5a2907a5395cc
VicWan <Vicwan1992@163.com>
2019-10-28 23:05:35 +0800

    Add 4Sum

--- algorithm/leetcode/Medium/15. 3Sum.py
+++ algorithm/leetcode/Medium/15. 3Sum.py
@@ -32,31 +32,31 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         if len(nums) < 3:
-            return []
+            return None
         res = []
         nums.sort()
-        for i in range(len(nums)-2):
-            if i > 0 and  nums[i] == nums[i - 1]: #注意，去重的要点
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
             left = i + 1
             right = len(nums) - 1
             while left < right:
                 sum = nums[i] + nums[left] + nums[right]
-                if sum > 0:
-                    right -= 1
-                elif sum < 0:
+                if sum < 0:
                     left += 1
+                elif sum > 0:
+                    right -= 1
                 else:
                     res.append([nums[i], nums[left], nums[right]])
-                    #注意，去重的要点
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
+                        right -=1
                     left += 1; right -= 1
         return res
                 
             
     
 s = Solution()
-print(s.threeSum([-1,0,1,2,-1,-4]))
+#print(s.threeSum([-1,0,1,2,-1,-1,-4,5]))
+print(s.threeSum([0,0,0]))
---
+++ algorithm/leetcode/Medium/18. 4Sum.py
@@ -0,0 +1,47 @@
+#leetcode 18. 4Sum
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 4:
+            return []
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i+1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = len(nums) - 1
+                while left < right:
+                    sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    if sum < target:
+                        left += 1
+                    elif sum > target:
+                        right -= 1
+                    else:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1; right -= 1
+        return res
+
+
+s = Solution()
+print(s.fourSum([1,0,-1,0,-2,2], 1)) 
+"""
+A solution set is:
+[
+    [-1,  0, 0, 1],
+    [-2, -1, 1, 2],
+    [-2,  0, 0, 2]
+]
+"""
